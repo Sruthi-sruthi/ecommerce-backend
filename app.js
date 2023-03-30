8c19ee18b9484950cc63013ac8f041ebd82ccc53
sruthi <sruthimadayi01@gmail.com>
2023-03-30 11:07:02 +0530

    completed flipkart project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.json());  
 app.use(express.static("./public"));
 app.use(cors())
 const connectDB = require("./config/config");
